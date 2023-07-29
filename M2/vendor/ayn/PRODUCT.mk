a6c9e1e9853c94d406ab19a58e241b7a83c919fe
AYN Tech <tech@ayn.hk>
2023-07-30 05:40:47 +0800

    Add Odin specific device configs

---
+++ M2/vendor/ayn/PRODUCT.mk
@@ -0,0 +1,24 @@
+$(warning "including ayn packages")
+PRODUCT_PACKAGES += pservice
+PRODUCT_PACKAGES += GameAssistant
+PRODUCT_PACKAGES += OdinSettings
+PRODUCT_PACKAGES += TouchMapping
+#PRODUCT_PACKAGES += GamepadTest
+PRODUCT_PACKAGES += FactoryTest
+#PRODUCT_PACKAGES += FPS_2D
+PRODUCT_PACKAGES += Gboard
+PRODUCT_PACKAGES += Chrome
+PRODUCT_PACKAGES += OdinLauncher
+#PRODUCT_PACKAGES += Upgrade
+PRODUCT_PACKAGES += WallpaperPicker3
+PRODUCT_PACKAGES += ScreenCapture
+PRODUCT_PACKAGES += SetupWizard
+
+CURRENT_BUILD=M2
+$(warning "=============PRODUCT.mk CURRENT_BUILD=$(CURRENT_BUILD)=============")
+ifeq ($(CURRENT_BUILD),M2)
+
+endif
+
+PRODUCT_PACKAGES += Q4Ota
+PRODUCT_PACKAGES += OdinDumpLog
